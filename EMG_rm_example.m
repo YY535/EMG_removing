92b88daa6d3d7e71aec308b7b9b6c91426638ca9
YY535 <chenww1990@gmail.com>
2019-02-25 16:18:43 +0100

    0225

--- EMG_rm_example.m
+++ EMG_rm_example.m
@@ -10,7 +10,7 @@ tmp_t = 9e6:9.5e6; % example period with high muscle tone
 % plot of spectrum, Ach recording and ripple power: 
 load ACh_NREM.mat
 opf1 = @(x)(bsxfun(@rdivide,x, std(x))); % not perticularily important.
-opf = @(x)(opf1(x-min(x)));
+opf = @(x)(opf1(bsxfun(@minus, x, min(x))));
 end_t = length(lfp.data)/1000*5;
 figure;
 plot([1:end_t]/5, bsxfun(@plus, [10 20], opf([ACh_NREM.signals.ACh(1:end_t), ACh_NREM.signals.SWRpower(1:end_t)])));
