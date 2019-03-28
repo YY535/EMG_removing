50b116c4659077df19b5cd40e54d0bf3c6c71bf3
YY535 <chenww1990@gmail.com>
2019-03-28 02:11:21 +0100

    affinefit

---
+++ util/SREaffineV.m
@@ -0,0 +1,37 @@
+function r = SREaffineV(x,y,varargin)
+% r = SREaffineV(x,y,varargin) 
+
+if nargin > 2
+    iscenter = varargin{1};
+else
+    iscenter = true;
+end
+[nt, ny] = size(y);
+if nt < ny
+    y = y';
+    [nt, ny] = size(y);
+end
+[nx, nch] = size(x);
+if nx ~= nt
+    if nt == nch
+        x = x';
+        nch = nx;
+    else
+        fprintf('size mismatch')
+    end
+end
+r = zeros(nch,ny);
+opf_x = @(x)(x'*x/(length(x)-1));
+for n = 1:ny 
+    tmp_y = y(:,n);
+for k = 1:nch
+    if iscenter
+        tmp_x = [ones(nt, 1), x(:,k)];
+        tmp_ch = 2;
+    else
+        tmp_x = x(:,k);
+        tmp_ch = 1;
+    end
+    r(k,n) = opf_x(tmp_y - tmp_x*((tmp_x'*tmp_x+1e-10*eye(tmp_ch))\(tmp_x'*tmp_y)));
+end
+end
