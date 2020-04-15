4fdb64f38164f8dd15cbe8abcdb8beb842534656
YY535 <chenww1990@gmail.com>
2020-04-15 16:54:52 +0200

    ER4.0rescale

--- EMG/EMG_au_rescale.m
+++ EMG/EMG_au_rescale.m
@@ -1,13 +1,14 @@
 function EMG_au_rescale(varargin)
-% to fix the rescaling problem
+% to fix the rescaling problem. 
+% please go to the directory or give the directory and run the function.
 % inputs: 
-%   savedir: the directory that one is gonna save the data. the default is
-%            the current directory. 
+%   savedir: string indicate the directory that one is gonna save the data. 
+%            the default is the current directory. 
 %   EMG_rm_file: e.g., 'FileBase.EMG_rm.sh*.mat'
+% 
 
 owd = pwd;
-[savedir,EMG_rm_file] = ...
-    DefaultArgs(varargin, {[],[]});
+[savedir,EMG_rm_file] = DefaultArgs(varargin, {[],[]});
 
 if ~isempty(savedir)
     cd(savedir)
@@ -22,8 +23,8 @@ else
 end
 nfiles = length(EMG_rm_files);
 
-for k = 1:nfiles
-    EMG_rm_file = EMG_rm_files(k).name;
+for kk = 1:nfiles
+    EMG_rm_file = EMG_rm_files(kk).name;
     load(EMG_rm_file, 'Ws','As','AW','EMG_au','armodel','sug_period','par','denoise_shank','LFPfile')
     
     nPeriod = size(sug_period,1);
@@ -36,30 +37,32 @@ for k = 1:nfiles
         scaling_factor = zeros(nPeriod,nshank);
     end
     
-    EMGFileName = cell(nshank,1);
+    EMGFileNames = cell(nshank,1);
     
     for n = 1:nshank
         myData = zeros(1,nt);
         
         for k = 1:nPeriod
             if ~isempty(As{k,n})
-                scaling_factor(k,n) = sign(sum(As{k,n}))*sqrt(As{k,n}'*As{k,n});
+                nch = length(As{k,n});
+                scaling_factor(k,n) = sign(sum(As{k,n}))*sqrt(As{k,n}'*As{k,n}/nch);
             end
             myData(sug_period(k,1):sug_period(k,2)) = scaling_factor(k,n)*EMG_au{k,n};
         end
         
         myData = int16(myData);
-        EMGFileName{n} = sprintf('%s%s.sh%d.emg',savedir,FileBase,denoise_shank(n));
-        if ~exist(EMGFileName{n},'file')
-            fileID = fopen(EMGFileName{n},'w');
+        EMGFileName = sprintf('%s%s.sh%d.emg',savedir,FileBase,denoise_shank(n));
+        if ~exist(EMGFileName,'file')
+            fileID = fopen(EMGFileName,'w');
             fwrite(fileID, myData,'int16');
             fclose(fileID);
             clear myData
         end
+        EMGFileNames{n} = EMGFileName;
     end
     
     save(EMG_rm_file, 'Ws','As','AW','EMG_au','armodel','sug_period','par','denoise_shank','LFPfile',...
-        'scaling_factor','EMGFileName')
+        'scaling_factor','EMGFileNames','savedir')
     
     fprintf('\n %s fixed',EMG_rm_file)
 end
